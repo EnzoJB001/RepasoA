19fe651209fb719b572b3cbcc3b448b05f1cd580
BaridonFrancisco <baridonfrancisco@gmail.com>
2023-08-14 11:09:45 -0300

    commitFrancisco

---
+++ src/repasoa/arrayNumeros.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repasoa;
+
+import java.awt.List;import java.util.ArrayList;
+
+/**
+ *
+ * @author Owner
+ */
+public class arrayNumeros {
+    public static void main(String[] args) {
+        ArrayList<Integer>nums=new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(5);
+        for(Integer num:nums){
+            System.out.println("num ->"+num);
+        }
+    }
+   
+}
